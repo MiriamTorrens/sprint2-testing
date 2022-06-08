be2654c7f60e0f7d2059561e6e449ef326c6c021
Miriam Torrens <m.torrens14@gmail.com>
2022-06-08 18:02:07 +0200

    finish

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 class Room {
     constructor(params){
-        this.name = params.name; //String
-        this.bookings = params.bookings; //Array de bookings
-        this.rate = params.rate; //Cent
-        this.discount = params.discount; //Int percentage
+        this.name = params.name; 
+        this.bookings = params.bookings; 
+        this.rate = params.rate; 
+        this.discount = params.discount;
     }
     isOccupied(date){
-        //Devuelve false si no está ocupada y devuelve el huésped si está ocupada
+    //Devuelve false si no está ocupada y devuelve el huésped si está ocupada
         const bookings = this.bookings;
         if(this.bookings.length){
             for(let i = 0; i < bookings.length; i++){
-                if(date >= bookings[i].checkin && date <= bookings[i].checkout){
-                    return bookings[i].name;
-                }
+                if(date >= bookings[i].checkin && date <= bookings[i].checkout) return bookings[i].name;
             }
             return false;
-        }else{
-            return false;
         }
+        return false;
     }
     occupancyPercentage(startDate, endDate){
-        //Devuelve el porcentaje de días con ocupación entre el rango de fechas proporcionado
+    //Devuelve el porcentaje de días con ocupación entre el rango de fechas proporcionado
         const bookings = this.bookings;
-        const daysOccupied = []; //Días de ocupación de cada booking
-        let totalDays; //Total de días de ocupación de todos los bookings
-        const daysDate = parseInt((endDate - startDate)/86400000); //Total de días a comprobar
+        const daysOccupied = []; //Array con los días de ocupación de cada booking
+        let totalDays; //Suma del total de días de ocupación de todos los bookings
+        const daysDate = parseInt((endDate - startDate)/86400000); //Rango de días a comprobar
         if(this.bookings.length){
             for(let i = 0; i < bookings.length; i++){
                 if(bookings[i].checkin >= startDate && bookings[i].checkin <= endDate){
@@ -34,22 +31,21 @@ class Room {
             }
             const percentage = ((totalDays*100)/daysDate) > 100 ? 100 : parseInt((totalDays*100)/daysDate);
             return percentage;
-        }else{
-            return 0;
         }
+        return 0;
     }
 }
 class Booking{
     constructor(params){
-        this.name = params.name //String
-        this.email = params.email //String
-        this.checkin = params.checkin //Date
-        this.checkout = params.checkout //Date
-        this.discount = params.discount //Int percentage
-        this.room = params.room //Object
+        this.name = params.name 
+        this.email = params.email 
+        this.checkin = params.checkin 
+        this.checkout = params.checkout 
+        this.discount = params.discount 
+        this.room = params.room 
     }
     getFee(){
-        //Devuelve tarifa incluyendo los descuentos de la habitación y del cliente
+    //Devuelve tarifa incluyendo los descuentos de la habitación y del cliente
         const rate = this.room.rate;
         const discountRoom = rate*(this.room.discount/100);
         const discountBooking = rate*(this.discount/100);
@@ -60,10 +56,10 @@ class Booking{
 }
 
 function totalOccupancyPercentage(rooms, startDate, endDate){
-    //Devuelve el porcentaje de ocupación total de las habitaciones que incluyamos en el array
-    const ocuppancyDays = [];
-    let totalDays;  //Total de días ocupados
-    const daysDate = parseInt((endDate - startDate)/86400000); //Días a comprobar
+//Devuelve el porcentaje de ocupación total de las habitaciones que incluyamos en el array
+    const ocuppancyDays = []; //Array con los días de ocupación de cada booking
+    let totalDays;  //Suma del total de días de ocupación de todos los bookings
+    const daysDate = parseInt((endDate - startDate)/86400000); ////Rango de días a comprobar
     for(let i = 0; i < rooms.length; i++){
         for(let j = 0; j < rooms[i].bookings.length; j++){
             const bookings = rooms[i].bookings[j];
@@ -78,7 +74,7 @@ function totalOccupancyPercentage(rooms, startDate, endDate){
 }
 
 function availableRooms(rooms, startDate, endDate){
-    //Devuelve las habitaciones que están disponibles en esas fechas
+//Devuelve las habitaciones que están disponibles en esas fechas
     const Available = [];
     for(let i = 0; i < rooms.length; i++){
         for(let j = 0; j < rooms[i].bookings.length; j++){
@@ -92,12 +88,8 @@ function availableRooms(rooms, startDate, endDate){
             }
         }
     }
-    if(Available.length){
-        return Available.toString();
-    }else{
-        return "No room available";
-    }
-    
+    if(Available.length) return Available.toString();
+    return "No room available";
 }
 
 module.exports = { Room, Booking, totalOccupancyPercentage, availableRooms }
